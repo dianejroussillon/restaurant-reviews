339a353f8a5f03f4014a47e90c2f50dfdb794200
diane <johnston>
2021-11-10 10:42:13 +0100

    nested ressources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :restaurants do
+  #   collection do
+  #     get :top   
+  #     # get /restaurants/top
+  #   end
+  #   member do
+  #     get :chef
+  #     # get /restaurants/:id/chef
+  #   end
+  # end
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
+
 end
+
+
+# reviews
+# new
+# create
+
+# get /restaurants/:id/reviews/new
+# post /restaurants/:id/reviews/
